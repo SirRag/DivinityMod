f7b5783e244c4dee3139d690f8bcf169ee12aa30
Caden (Sir Rag) <41925028+SirRag@users.noreply.github.com>
2018-07-31 13:00:28 -0500

    Update SancheziumBow.cs

--- Divinity/Items/Sanchezium/SancheziumBow.cs
+++ Divinity/Items/Sanchezium/SancheziumBow.cs
@@ -19,6 +19,7 @@ namespace Divinity.Items.Sanchezium
             item.damage = 44;
             item.noMelee = true;
             item.ranged = true;
+            item.scale = 0.8f;
             item.width = 16;
             item.height = 36;
             item.useTime = 30;
@@ -28,18 +29,23 @@ namespace Divinity.Items.Sanchezium
             item.useAmmo = AmmoID.Arrow;
             item.knockBack = 1;
             item.value = 1000;
-            item.rare = 11;
+            item.rare = 4;
             item.UseSound = SoundID.Item5;
             item.autoReuse = true;
             item.shootSpeed = 10f;
-
         }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(0, 0);
+        }
+
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
             Projectile.NewProjectile(position.X, position.Y, speedX, speedY, ProjectileID.HolyArrow, damage, knockBack, player.whoAmI, 0f, 0f); //This is spawning a projectile of type FrostburnArrow using the original stats
             return false; //Makes sure to not fire the original projectile
-        }
-		
+        }		
+
 		public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
